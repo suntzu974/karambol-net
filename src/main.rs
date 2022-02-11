0af9429e18f8fb5e3ac8e91dde5ec8c25e92cd60
Jeannick GRONDIN <jeannick.grondin@gmail.com>
2022-02-11 07:29:53 +0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use mobc::{Connection, Pool};
+use mobc_postgres::{tokio_postgres, PgConnectionManager};
+use std::convert::Infallible;
+use tokio_postgres::NoTls;
+use warp::{Filter, Rejection};
+
+mod data;
+mod db;
+mod error;
+mod handler;
+
+type Result<T> = std::result::Result<T, Rejection>;
+type DBCon = Connection<PgConnectionManager<NoTls>>;
+type DBPool = Pool<PgConnectionManager<NoTls>>;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    // https://github.com/seanmonstar/pretty-env-logger
+    let ssl_key = std::env::var("SSL_KEY").expect("SSL KEY must be set");
+    let ssl_crt = std::env::var("SSL_CRT").expect("SSL CRTY must be set");
+
+    let db_pool = db::create_pool().expect("database pool can be created");
+
+    db::init_db(&db_pool)
+        .await
+        .expect("database can be initialized");
+
+    let health_route = warp::path!("health")
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::health_handler);
+
+    let todo = warp::path("todo");
+    let todo_routes = todo
+        .and(warp::get())
+        .and(warp::query())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::list_todos_handler)
+        .or(todo
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::create_todo_handler))
+        .or(todo
+            .and(warp::put())
+            .and(warp::path::param())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::update_todo_handler))
+        .or(todo
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::delete_todo_handler));
+
+    let routes = health_route
+        .or(todo_routes)
+        .with(warp::cors().allow_any_origin())
+        .recover(error::handle_rejection);
+
+        warp::serve(routes)
+        .tls().cert_path(ssl_crt)
+        .key_path(ssl_key).run(([0,0,0,0],8443)).await;
+}
+
+fn with_db(db_pool: DBPool) -> impl Filter<Extract = (DBPool,), Error = Infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
+}
